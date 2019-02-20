ce71d6043b268b4385972a88952acb86fc906c78
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-02-20 10:00:15 +0200

    add: local data source

---
+++ app/src/main/java/com/oleksandr/havryliuk/todo_list/data/source/local/TasksLocalDataSource.java
@@ -0,0 +1,180 @@
+package com.oleksandr.havryliuk.todo_list.data.source.local;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.oleksandr.havryliuk.todo_list.data.Task;
+import com.oleksandr.havryliuk.todo_list.data.source.TasksDataSource;
+import com.oleksandr.havryliuk.todo_list.utils.AppExecutors;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class TasksLocalDataSource implements TasksDataSource {
+
+    private static volatile TasksLocalDataSource INSTANCE;
+
+    private TasksDao mTasksDao;
+
+    private AppExecutors mAppExecutors;
+
+    // Prevent direct instantiation.
+    private TasksLocalDataSource(@NonNull AppExecutors appExecutors,
+                                 @NonNull TasksDao tasksDao) {
+        mAppExecutors = appExecutors;
+        mTasksDao = tasksDao;
+    }
+
+    public static TasksLocalDataSource getInstance(@NonNull Context context) {
+        if (INSTANCE == null) {
+            synchronized (TasksLocalDataSource.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new TasksLocalDataSource(new AppExecutors(),
+                            ToDoDatabase.getInstance(context).taskDao());
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    public void getTasks(@NonNull final LoadTasksCallback callback) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                final List<Task> tasks = mTasksDao.getTasks();
+                mAppExecutors.mainThread().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (tasks.isEmpty()) {
+                            // This will be called if the table is new or just empty.
+                            callback.onDataNotAvailable();
+                        } else {
+                            callback.onTasksLoaded(tasks);
+                        }
+                    }
+                });
+            }
+        };
+
+        mAppExecutors.diskIO().execute(runnable);
+    }
+
+    @Override
+    public void getTask(@NonNull final String taskId, @NonNull final GetTaskCallback callback) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                final Task task = mTasksDao.getTaskById(taskId);
+
+                mAppExecutors.mainThread().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (task != null) {
+                            callback.onTaskLoaded(task);
+                        } else {
+                            callback.onDataNotAvailable();
+                        }
+                    }
+                });
+            }
+        };
+
+        mAppExecutors.diskIO().execute(runnable);
+    }
+
+    @Override
+    public void saveTask(@NonNull final Task task) {
+        checkNotNull(task);
+        Runnable saveRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.insertTask(task);
+            }
+        };
+        mAppExecutors.diskIO().execute(saveRunnable);
+    }
+
+    @Override
+    public void completeTask(@NonNull final Task task) {
+        Runnable completeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.updateCompleted(task.getId(), true);
+            }
+        };
+
+        mAppExecutors.diskIO().execute(completeRunnable);
+    }
+
+    @Override
+    public void completeTask(@NonNull String taskId) {
+        // Not required for the local data source because the TasksRepository handles
+        // converting from a taskId to a task using its cached data.
+    }
+
+    @Override
+    public void activateTask(@NonNull final Task task) {
+        Runnable activateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.updateCompleted(task.getId(), false);
+            }
+        };
+        mAppExecutors.diskIO().execute(activateRunnable);
+    }
+
+    @Override
+    public void activateTask(@NonNull String taskId) {
+        // Not required for the local data source because the TasksRepository handles
+        // converting from a taskId to a task using its cached data.
+    }
+
+    @Override
+    public void clearCompletedTasks() {
+        Runnable clearTasksRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.deleteCompletedTasks();
+
+            }
+        };
+
+        mAppExecutors.diskIO().execute(clearTasksRunnable);
+    }
+
+    @Override
+    public void refreshTasks() {
+        // Not required because the TasksRepository handles the logic of refreshing the
+        // tasks from all the available data sources.
+    }
+
+    @Override
+    public void deleteAllTasks() {
+        Runnable deleteRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.deleteTasks();
+            }
+        };
+
+        mAppExecutors.diskIO().execute(deleteRunnable);
+    }
+
+    @Override
+    public void deleteTask(@NonNull final String taskId) {
+        Runnable deleteRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mTasksDao.deleteTaskById(taskId);
+            }
+        };
+
+        mAppExecutors.diskIO().execute(deleteRunnable);
+    }
+
+    static void clearInstance() {
+        INSTANCE = null;
+    }
+}
